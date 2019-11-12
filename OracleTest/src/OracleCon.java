4b84f9638097d1e5c257fd97b8dfd859028e0560
João Henrique Almeida Santos <49997926+JoaoHenriqueAS@users.noreply.github.com>
2019-11-12 11:01:32 -0300

    Atualizado parâmetros

--- OracleTest/src/OracleCon.java
+++ OracleTest/src/OracleCon.java
@@ -10,14 +10,14 @@ public class OracleCon {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
 			//step 2 - Criar objeto connection
-			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//10.32.4.51:1521/BDTPPRC","ELO000500","joaoh");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//[host]:[port]/db","user","pass");
 			
 			//step 3 - Criar objeto statement
 			Statement stmt = con.createStatement();
 			
 			//step 4 - executar query
 			ResultSet rs = stmt.executeQuery("Select Count(*) "
-					+ "From Clc.Tbclcr_Crsa_Crto_Fsco_Dnmc "
+					+ "From tablename "
 					+ "Where Dt_Locl_Trns = '25/05/19';");
 			while(rs.next()) {
 				System.out.println(rs.getInt("COUNT(*)"));
